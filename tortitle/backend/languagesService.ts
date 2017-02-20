6080698553839c78a6c7c4371a31b722eea9b393
Slawomir Gnatonski <slawomir.gnatonski@ciber.com>
2017-02-20 15:40:42 +0100

    fixed interface for querying language entities

--- tortitle/backend/languagesService.ts
+++ tortitle/backend/languagesService.ts
@@ -1,8 +1,11 @@
 ﻿import * as azure from "azure-storage";
 import { default as cache } from "./cache";
 import { Entities } from "./Entities";
-import { ISubtitleEntity } from "./subtitle";
 import { Iso639, ILanguage } from "../iso639";
+
+interface ILanguageEntity {
+    RowKey: string;
+}
 
 export module LanguagesService {
     export async function getCachedLanguages() {
@@ -17,7 +20,7 @@ export module LanguagesService {
 
     export async function getLanguages() {
         const languagesTableName = "languages";
-        const subs = await Entities.queryEntities<ISubtitleEntity>(languagesTableName, new azure.TableQuery());
+        const subs = await Entities.queryEntities<ILanguageEntity>(languagesTableName, new azure.TableQuery());
         const langs = new Set(subs.map(i => i.RowKey));
         const availableLangs = Iso639.languages.filter(x => langs.has(x.code));
         return availableLangs;
